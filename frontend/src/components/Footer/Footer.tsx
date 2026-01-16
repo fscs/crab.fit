11c28bc0fe11312556af869bc9ff06d10119b2ba
einetuer <1444@wienstroer.net>
2026-01-16 22:17:49 +0100

    privacy-policy.patch

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -10,26 +10,13 @@ interface FooterProps {
   isSmall?: boolean
 }
 
-const Footer = async ({ isSmall }: FooterProps) => {
-  const { t } = await useTranslation('common')
-  const isRunningInApp = headers().get('referer')?.includes('android-app://fit.crab')
-
-  return isRunningInApp
-    ? null // Cannot show external donation link in an Android app
-    : <footer
-      id="donate" // Required to allow scrolling directly to the footer
-      className={makeClass(styles.footer, isSmall && styles.small)}
-    >
-      <span>{t('donate.info')}</span>
-      <Button
-        isSmall
-        title={t('donate.title')}
-        href="https://ko-fi.com/A06841WZ"
-        target="_blank"
-        rel="noreferrer noopener payment"
-        style={{ whiteSpace: 'nowrap' }}
-      >{t('donate.button')}</Button>
-    </footer>
-}
+const Footer = async ({ isSmall }: FooterProps) => (
+  <footer
+    id="donate"
+    className={makeClass(styles.footer, isSmall && styles.small)}
+  >
+    <a href="https://static.hhu-fscs.de/datenschutz-crabfit">Privacy Notice</a>
+  </footer>
+)
 
 export default Footer
